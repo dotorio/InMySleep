93a3135267bc0571a1296a73aca5ff13dde23737
윤지호 <hoyunji@naver.com>
2024-08-30 14:57:08 +0900

    [BE] feat: Initial Project

---
+++ backend/in-my-sleep/src/main/java/com/inmysleep/backend/InMySleepApplication.java
@@ -0,0 +1,13 @@
+package com.inmysleep.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InMySleepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InMySleepApplication.class, args);
+	}
+
+}
