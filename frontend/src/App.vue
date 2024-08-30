c7eff3b664b22a66b179e08df08a41867e1dd0f5
어지민 <djwlalssla@naver.com>
2024-08-30 17:32:57 +0900

    [FE] feat: 메인 페이지 기초 작업 -nav바 생성 -카테고리(공지, NFT 전시관 등) 생성 -footer 생성

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import Nav from "@/components/Nav.vue";
+import "@/assets/css/main.css";
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ frontend/src/components/Nav.vue
@@ -0,0 +1,36 @@
+<script setup></script>
+
+<template>
+  <div class="nav-con flex-align">
+    <div class="left-side">
+      <img src="/src/assets/home/logo.gif" alt="logo" class="logo" />
+    </div>
+
+    <div class="right-side">
+      <span class="bit-t account">로그인</span>
+      <span class="bit-t account">회원가입</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.nav-con {
+  height: 100px;
+  justify-content: space-between;
+  background-color: #1f1a59;
+}
+.logo {
+  width: 100px;
+  margin-left: 10px;
+}
+.account {
+  font-size: 25px;
+  color: white;
+  margin-right: 20px;
+
+  cursor: pointer;
+}
+.account:hover {
+  color: #6f63f5;
+}
+</style>
