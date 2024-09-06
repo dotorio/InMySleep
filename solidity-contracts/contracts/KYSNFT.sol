9e8df30934dd5ee4df4d1788454328e63ce11245
이준수 <skymoon28@nate.com>
2024-09-06 19:51:33 +0900

    [BC] feat: NFT sol 파일 작성

---
+++ solidity-contracts/contracts/KYSNFT.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract KYSNFT is ERC721Enumerable, ERC721URIStorage, Ownable {
+    constructor(
+        address initialOwner
+    ) ERC721("In My Sleep Easter Eggs", "IMS") Ownable(initialOwner) {}
+
+    function mint(
+        address to,
+        string memory _tokenURI
+    ) public onlyOwner returns (uint256) {
+        uint256 newTokenId = totalSupply();
+        _mint(to, newTokenId);
+        _setTokenURI(newTokenId, _tokenURI);
+
+        return newTokenId;
+    }
+
+    function burn(uint256 tokenId) internal {
+        _burn(tokenId);
+        _setTokenURI(tokenId, "");
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function tokensOfOwner(
+        address owner
+    ) public view returns (uint256[] memory) {
+        uint256 tokenCount = balanceOf(owner);
+        uint256[] memory tokenIds = new uint256[](tokenCount);
+
+        for (uint256 i = 0; i < tokenCount; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex(owner, i);
+        }
+
+        return tokenIds;
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721Enumerable, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _increaseBalance(
+        address account,
+        uint128 value
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._increaseBalance(account, value);
+    }
+
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal override(ERC721, ERC721Enumerable) returns (address) {
+        return super._update(to, tokenId, auth);
+    }
+}
