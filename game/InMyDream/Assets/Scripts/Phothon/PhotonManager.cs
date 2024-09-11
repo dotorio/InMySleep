d49d09b98ab2d68510f1eb210606061e2244ce0f
김영수 <kimyeong3732@naver.com>
2024-09-11 17:02:37 +0900

    [GAME] feat: 최종 photon 코드 작성
    Former-commit-id: a608e29d9003878d77110f68650f8cc6cab0a6e2

--- game/InMyDream/Assets/Scripts/Phothon/PhotonManager.cs
+++ game/InMyDream/Assets/Scripts/Phothon/PhotonManager.cs
@@ -3,67 +3,96 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using ExitGames.Client.Photon;
 
 public class PhotonManager : MonoBehaviourPunCallbacks
 {
     private readonly string version = "1.0f";
-    private string userId = "KHJ";
+    private int userId = UserData.instance.userId;
+    private string userName = UserData.instance.userName;
 
     private void Awake()
     {
         PhotonNetwork.AutomaticallySyncScene = true;
         PhotonNetwork.GameVersion = version;
-        PhotonNetwork.NickName = userId;
+        PhotonNetwork.NickName = userName;
 
-        Debug.Log(PhotonNetwork.SendRate);      // 포톤 서버와 통신 횟수 (30)
-        PhotonNetwork.ConnectUsingSettings();   // 서버 접속
+        PhotonNetwork.ConnectUsingSettings();
     }
 
     public override void OnConnectedToMaster()
     {
-        Debug.Log("Connected To Master!");
         PhotonNetwork.JoinLobby();
     }
 
     public override void OnJoinedLobby()
     {
-        base.OnJoinedLobby();
-        Debug.Log($"Connected To Lobby = {PhotonNetwork.InLobby}");
-        PhotonNetwork.JoinRandomRoom();
-    }
-
-    public override void OnJoinRandomFailed(short returnCode, string message)
-    {
-        Debug.Log($"Join Failed {returnCode} : {message}");
-
-        // 룸 속성 정의
         RoomOptions ro = new RoomOptions();
-        ro.MaxPlayers = 20;
+        ro.MaxPlayers = 2;
         ro.IsOpen = true;
         ro.IsVisible = true;
 
-        PhotonNetwork.CreateRoom("My Room", ro);
+        PhotonNetwork.CreateRoom(userName, ro);
     }
 
-    public override void OnCreatedRoom()
-    {
-        Debug.Log("Created Room!");
-        Debug.Log($"Room Name = {PhotonNetwork.CurrentRoom.Name}");
-    }
+    //public override void OnJoinRandomFailed(short returnCode, string message)
+    //{
+        
+    //}
 
     public override void OnJoinedRoom()
     {
-        Debug.Log($"In Room = {PhotonNetwork.InRoom}");
-        Debug.Log($"Player Count = {PhotonNetwork.CurrentRoom.PlayerCount}");
+        PhotonNetwork.LocalPlayer.CustomProperties["character"] = "Player1";
+        PhotonNetwork.LocalPlayer.CustomProperties["material"] = "00";
+    }
 
-        foreach (var player in PhotonNetwork.CurrentRoom.Players)
+    public void OnSelectCharacter(string characterName, string characterMaterial)
+    {
+        // 중복 캐릭터 선택 확인
+        ExitGames.Client.Photon.Hashtable playerProps = new ExitGames.Client.Photon.Hashtable();
+        playerProps["character"] = characterName;
+        playerProps["material"] = characterMaterial;
+        PhotonNetwork.LocalPlayer.SetCustomProperties(playerProps);
+
+        // 룸의 유저 id 저장
+        ExitGames.Client.Photon.Hashtable roomProps = new ExitGames.Client.Photon.Hashtable();
+        roomProps[PhotonNetwork.LocalPlayer.ActorNumber.ToString()] = userId;
+        PhotonNetwork.CurrentRoom.SetCustomProperties(roomProps);
+    }
+
+    public void StartGame()
+    {
+        if (PhotonNetwork.IsMasterClient)
         {
-            Debug.Log($"{player.Value.NickName}, {player.Value.ActorNumber}");
+            // 인원 수가 차있는지 확인
+            if(PhotonNetwork.PlayerList.Length != 2)
+            {
+                Debug.Log("인원이 부족합니다.");
+                return;
+            }
+
+            // 모든 플레이어가 캐릭터를 선택했는지 확인
+            foreach (Player player in PhotonNetwork.PlayerList)
+            {
+                if (!player.CustomProperties.ContainsKey("character"))
+                {
+                    Debug.Log("모든 플레이어가 캐릭터를 선택하지 않았습니다.");
+                    return;
+                }
+            }
+
+            // 플레이어가 선택한 캐릭터가 중복인지 확인
+            object user1 = PhotonNetwork.PlayerList[0].CustomProperties["character"];
+            object user2 = PhotonNetwork.PlayerList[1].CustomProperties["character"];
+
+            if(user1.Equals(user2))
+            {
+                Debug.Log("캐릭터가 중복됩니다.");
+                return;
+            }
+
+            // 씬 로드
+            PhotonNetwork.LoadLevel("kysTest");
         }
-
-        Transform[] points = GameObject.Find("CreatePlayerGroup").GetComponentsInChildren<Transform>();
-        int idx = Random.Range(1, points.Length);
-
-        PhotonNetwork.Instantiate("Player1", points[idx].position, points[idx].rotation, 0);
     }
 }
