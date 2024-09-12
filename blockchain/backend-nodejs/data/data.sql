56de378ae19f87eda434709fee5e469748a40489
이준수 <skymoon28@nate.com>
2024-09-12 17:58:09 +0900

    [BC] feat: DB 테이블 수정 및 dump 파일 추가

--- blockchain/backend-nodejs/data/data.sql
+++ blockchain/backend-nodejs/data/data.sql
@@ -1,48 +1,91 @@
-create table users (
-    id int primary key auto_increment,
-	user_id varchar(255) unique,
-    wallet_address varchar(255) unique,
-    created_at timestamp default current_timestamp,
-    updated_at timestamp default current_timestamp on update current_timestamp
-);
+CREATE DATABASE  IF NOT EXISTS `nft` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `nft`;
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: nft
+-- ------------------------------------------------------
+-- Server version	8.4.1
 
-create table metadata (
-    id int primary key auto_increment,
-    metadata_uri varchar(255),
-    image_url varchar(255),
-    description TEXT,
-    attributes JSON,
-    created_at timestamp default current_timestamp
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-create table nfts (
-    id int primary key auto_increment,
-    contract_address varchar(255),
-    owner_address varchar(255),
-    metadata_id int,
-    token_type ENUM('ERC-721', 'ERC-1155'),
-	created_at timestamp default current_timestamp,
-    updated_at timestamp default current_timestamp on update current_timestamp,
-    foreign key (metadata_id) references metadata(id) on delete restrict
-);
+--
+-- Table structure for table `metadata`
+--
 
-INSERT INTO users (user_id, wallet_address)
-VALUES 
-('user_1', '0xFbE4946eB5d9f194C256A45a8BAc8faDa5Daa338'),
-('user_2', '0x0F00caaCb2AEBB3A7bf44543192CC1F2919774b7'),
-('user_3', '0xfc48396440e49bF9BbbcfBc1Bf402e9B15c06F75');
+DROP TABLE IF EXISTS `metadata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `metadata` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `metadata_uri` varchar(255) NOT NULL,
+  `image_url` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `attributes` json NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `metadata_uri_UNIQUE` (`metadata_uri`),
+  UNIQUE KEY `image_url_UNIQUE` (`image_url`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO metadata (metadata_uri, image_url, description, attributes)
-VALUES
-('ipfs://QmYuvDM8SBfX6nUXDuKtFhkw69jU3qTrW66RkMRNwUr3fv', 'ipfs://QmQN33ZfowrA9VFTLTUgyGnLNYQwmRxnYBWdiTy7MYPprW', 'This is NFT #1', '{"trait_type": "Background", "value": "Red"}'),
-('ipfs://QmRFJkBhS8yiQbJakUr2cpyTACiE5V57UEArBqM95GVSh3', 'ipfs://QmZHnCCsJAPAJXQ2Ef8tugZosGkXUdZutZ248JzTCmeU9h', 'This is NFT #2', '{"trait_type": "Eyes", "value": "Laser"}'),
-('ipfs://QmTbrJNKcQaYGm7bN6fnJkGjWS175ik8ngXRf4FwUTj5Rv', 'ipfs://QmUwcxXCWZ4BHn8SeQyjdkaqmoNtDzSm28T8XB26ye7TQQ', 'This is NFT #3', '{"trait_type": "Fur", "value": "Golden"}'),
-('ipfs://QmdcsjSdUSzKED5BrrzDDXhKxJcxhHrqh2pJL63SMZQrb4', 'ipfs://Qmb4Q1kkw2GmQh6bCBaX2Z8d23AvLuWorQLN7daQsHpPHN', 'This is NFT #4', '{"trait_type": "4", "value": "4"}'),
-('ipfs://QmSi8NNDeLAbTVVnmsm8qh1pumtF3wQzgDjzCm9NuZ482n', 'ipfs://QmQLv1mHxMTrgw9tZnCKLZNdpdP1yZYPYtaGgteJy9npFH', 'This is NFT #5', '{"trait_type": "5", "value": "5"}'),
-('ipfs://QmWmtZfCM4KdL9CN6SZNyEYxF4macLqPnjSzY3T3s5qhFi', 'ipfs://Qmbj9Au5hz7kZTDwwyDmKtHrHoXZVLEnmwJtMYqDkmAWD8', 'This is NFT #6', '{"trait_type": "6", "value": "6"}');
+--
+-- Table structure for table `nfts`
+--
 
--- INSERT INTO nfts (contract_address, owner_address, metadata_id, token_type)
--- VALUES
--- ('0xcontract1', '0xabc123456789abcdef123456789abcdef1234567', 1, 'ERC-721'),
--- ('0xcontract2', '0xdef123456789abcdef123456789abcdef1234567', 2, 'ERC-1155'),
--- ('0xcontract3', '0xghi123456789abcdef123456789abcdef1234567', 3, 'ERC-721');
+DROP TABLE IF EXISTS `nfts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `nfts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `token_id` int NOT NULL,
+  `contract_address` varchar(255) NOT NULL,
+  `owner_address` varchar(255) NOT NULL,
+  `metadata_id` int NOT NULL,
+  `token_type` enum('ERC-721','ERC-1155') NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `token_id_UNIQUE` (`token_id`),
+  KEY `nfts_ibfk_1` (`metadata_id`),
+  CONSTRAINT `nfts_ibfk_1` FOREIGN KEY (`metadata_id`) REFERENCES `metadata` (`id`) ON DELETE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(255) NOT NULL,
+  `wallet_address` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`),
+  UNIQUE KEY `wallet_address` (`wallet_address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-09-12 14:43:36
