9358d692c817849ae57d73ea867965d5a3ff30fd
이준수 <skymoon28@nate.com>
2024-09-23 12:26:11 +0900

    [BC] feat: SSL 추가

--- blockchain/backend-nodejs/src/config.ts
+++ blockchain/backend-nodejs/src/config.ts
@@ -2,11 +2,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const port = process.env.PORT || 3000;
-export const dbHost = process.env.DB_HOST || 'localhost';
-export const dbUser = process.env.DB_USER || 'root';
-export const dbPassword = process.env.DB_PASSWORD || 'root'; 
-export const dbName = process.env.DB_NAME || 'nft';
-export const web3Provider = process.env.WEB3_PROVIDER || 'http://localhost:7545';
+export const port = process.env.PORT || '';
+export const sslFilename = process.env.SSL_FILENAME || '';
+export const sslPassword = process.env.SSL_PASSWORD || '';
+export const dbHost = process.env.DB_HOST || '';
+export const dbUser = process.env.DB_USER || '';
+export const dbPassword = process.env.DB_PASSWORD || ''; 
+export const dbName = process.env.DB_NAME || '';
+export const web3Provider = process.env.WEB3_PROVIDER || '';
 export const contractAddress = process.env.CONTRACT_ADDRESS || '';
-export const jwtSecret = process.env.JWT_SECRET || 'secret';
+export const jwtSecret = process.env.JWT_SECRET || '';
--- blockchain/backend-nodejs/src/server.ts
+++ blockchain/backend-nodejs/src/server.ts
@@ -1,14 +1,23 @@
 import app from './app';
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
 import { connectDB } from './db/dbConnection';
-import { port } from './config';
+import { port, sslFilename, sslPassword } from './config';
+
+const options = {
+  pfx: fs.readFileSync(path.join(__dirname, sslFilename)),
+  passphrase: sslPassword,
+}
 
 const startServer = async () => {
   try {
     await connectDB();
-    
-    app.listen(port, () => {
+
+    https.createServer(options, app).listen(port, () => {
       console.log(`Server is running on port ${port}`);
-    });
+    })
+    
   } catch (err) {
     console.error('Error starting server: ', err);
   }
