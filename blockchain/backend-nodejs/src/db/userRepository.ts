de6fd2bf4ce5f4a33aa9b80af1c55c820887e13a
이준수 <skymoon28@nate.com>
2024-09-23 10:45:01 +0900

    [BC] fix: MetaMask 로그인 수정

--- blockchain/backend-nodejs/src/db/userRepository.ts
+++ blockchain/backend-nodejs/src/db/userRepository.ts
@@ -3,7 +3,7 @@ import { connectDB } from './dbConnection';
 // export const tmpLogin = async (userId: string, password: string): Promise<any> => {
 //     try {
 //         const conn = await connectDB();
-//         const [rows]: [any[], FieldPacket[]] = await conn.query('SELECT * FROM users WHERE user_id = ? AND password = ?', [userId, password]);
+//         const [rows]: [any[], FieldPacket[]] = await conn.query('SELECT * FROM user WHERE user_id = ? AND password = ?', [userId, password]);
 //         if (rows.length === 0) {
 //             throw new Error('Invalid credentials');
 //         }
@@ -18,7 +18,7 @@ import { connectDB } from './dbConnection';
 export const saveWalletToDB = async (address: string, username: string): Promise<void> => {
     try {
         const conn = await connectDB();
-        const [rows] = await conn.query('UPDATE users SET wallet_address = ? where username = ?', [address, username]);
+        const [rows] = await conn.query('UPDATE user SET wallet_address = ? where username = ?', [address, username]);
         console.log('Saved wallet to DB:', rows);
     } catch (error) {
         console.error('Error saving wallet to DB:', error);
