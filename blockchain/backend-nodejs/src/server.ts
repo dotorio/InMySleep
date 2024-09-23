4580dd7350461fbcf8dc5dc297902999b9160e13
이준수 <skymoon28@nate.com>
2024-09-23 15:13:06 +0900

    [BC] feat: SSL 인증서 테스트1

--- blockchain/backend-nodejs/src/server.ts
+++ blockchain/backend-nodejs/src/server.ts
@@ -14,10 +14,6 @@ const startServer = async () => {
   try {
     await connectDB();
 
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
-
     https.createServer(options, app).listen(port, () => {
       console.log(`Server is running on port ${port}`);
     })
