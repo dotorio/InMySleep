8dce7f5ea6a1a86079100490b4d6967c7fdf1300
이준수 <skymoon28@nate.com>
2024-09-26 02:43:45 +0900

    [FE] feat: 로컬 SSL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  server: {
+    https: {
+      key: fs.readFileSync("src/privkey1.pem"),
+      cert: fs.readFileSync("src/cert1.pem"),
+      ca: fs.readFileSync("src/chain1.pem"),
+    },
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
