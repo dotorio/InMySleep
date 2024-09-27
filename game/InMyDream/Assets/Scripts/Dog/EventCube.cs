09ddfeaad5837f9f7571e693405aac8179bcba75
윤지호 <hoyunji@naver.com>
2024-09-27 14:13:21 +0900

    [GAME] feat: 개 이벤트 위치로 이동
    Former-commit-id: f4fe37ff231e818e74ed83f631afbdb3285f7ca1

--- game/InMyDream/Assets/Scripts/Dog/EventCube.cs
+++ game/InMyDream/Assets/Scripts/Dog/EventCube.cs
@@ -4,21 +4,48 @@ using UnityEngine;
 
 public class EventCube : MonoBehaviour
 {
-    public DogMovement dogMovement; // 개의 움직임을 제어하는 스크립트 (Inspector에서 설정)
+    [Tooltip("개 캐릭터에 연결된 DogMovement 스크립트.")]
+    public DogMovement dogMovement;
 
-    void Update()
+    private void Start()
     {
-        // E 키를 눌렀을 때 개가 이 큐브의 위치로 이동
-        if (Input.GetKeyDown(KeyCode.E))
+        if (dogMovement == null)
         {
-            SetEventLocation();
+            // 개가 DogMovement 스크립트를 가지고 있는 오브젝트를 찾아 자동으로 할당
+            GameObject dog = GameObject.FindGameObjectWithTag("Dog");
+            if (dog != null)
+            {
+                dogMovement = dog.GetComponent<DogMovement>();
+            }
+
+            if (dogMovement == null)
+            {
+                Debug.LogWarning("DogMovement 스크립트를 찾을 수 없습니다.");
+            }
         }
     }
 
-    void SetEventLocation()
+    private void OnCollisionEnter(Collision collision)
     {
-        // 큐브의 위치를 event location으로 설정하고 개가 해당 위치로 이동하도록 트리거
-        dogMovement.eventLocation = this.transform;
-        dogMovement.OnEventTriggered();
+        Debug.Log("충돌 체크");
+        // 장애물 태그를 "Obstacle"로 설정했다고 가정
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Vector3 eventPosition = collision.contacts[0].point;
+            if (dogMovement != null)
+            {
+                Debug.Log("소음 이벤트");
+                dogMovement.OnEventTriggered(eventPosition);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("소음 이벤트");
+            dogMovement.OnEventTriggered(this.transform.position);
+        }
     }
 }
