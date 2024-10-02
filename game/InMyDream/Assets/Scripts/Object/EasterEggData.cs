42f16c4bccf33b47ee927bc5de38dc7377d5847f
이준수 <skymoon28@nate.com>
2024-10-02 16:25:07 +0900

    [GAME] feat: 이스터에그 API 요청
    Former-commit-id: c10ef0b03a706d115d7506fda1ff975a42e14240

--- game/InMyDream/Assets/Scripts/Object/EasterEggData.cs
+++ game/InMyDream/Assets/Scripts/Object/EasterEggData.cs
@@ -7,40 +7,97 @@ using UnityEngine.Networking;
 public class EasterEggData : MonoBehaviour
 {
     private string character;
+<<<<<<< Updated upstream
+=======
+    private int skin;
+    private string addEasterUrl = "https://j11e107.p.ssafy.io:8000/api/v1/easter/add-skin";
+
+
+    [System.Serializable]
+    public class ServerResponse
+    {
+        public bool success;    // 로그인 성공 여부
+        public EasterEggInfo data;    // 유저 정보가 포함된 "data" 필드
+        public string message;   // 서버에서 반환된 메시지
+    }
+
+    [System.Serializable]
+    public class Attributes
+    {
+        public int color;
+        public string character;
+    }
+
+    [System.Serializable]
+    public class EasterEggInfo
+    {
+        public string skinImgUrl;
+        public string description;
+        public Attributes attributes;
+        public bool duplicated;
+    }
+
+    [System.Serializable]
+    public class UserData
+    {
+        public int userId;
+
+        public UserData(int userId)
+        {
+            this.userId = userId;
+        }
+    }
+
+    private void Start()
+    {
+        /*        int easterData = (int)PhotonNetwork.CurrentRoom.CustomProperties["EasterEggData"];
+
+                if (easterData <= 5)
+                {
+                    character = "Bear";
+                    skin = easterData;
+                }
+                else
+                {
+                    character = "Bunny";
+                    skin = easterData - 5;
+                }*/
+    }
+>>>>>>> Stashed changes
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            //StartCoroutine(AddEasterEgg());
+            StartCoroutine(AddEasterEgg());
             Debug.Log("이스터 에그 획득 성공");
-            Destroy(gameObject);
         }
     }
 
     // 임시 코드, api 완성시 수정
     IEnumerator AddEasterEgg()
     {
+        Debug.Log("Start AddEasterEgg");
         // 로그인 정보를 JSON 형식으로 준비
-        LoginData loginData = new LoginData("a", "a");
-        string jsonData = JsonUtility.ToJson(loginData);
+        /*int userId = UserData.instance.userId;*/
+        UserData userData = new UserData(45);
+        string jsonData = JsonUtility.ToJson(userData);
+/*        int userId = 45;
+        string jsonData = $"{{\"userId\":{userId}}}";*/
 
         // UnityWebRequest로 HTTP POST 요청을 준비
-        UnityWebRequest request = new UnityWebRequest("a", "POST");
+        UnityWebRequest request = new UnityWebRequest(addEasterUrl, "POST");
         byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
         request.uploadHandler = new UploadHandlerRaw(bodyRaw);
         request.downloadHandler = new DownloadHandlerBuffer();
         request.SetRequestHeader("Content-Type", "application/json");
 
         // 서버로부터 응답을 받을 때까지 대기
-        yield return request.SendWebRequest();
 
+        yield return request.SendWebRequest();
         // 요청 결과 처리
         if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
         {
-            // 로그인 실패 시 오류 메시지 표시
-            //errorMessageText.text = "로그인 실패: " + response.message;
-            //ShowLoginFailPopup();  // 로그인 실패 팝업창 띄우기
 
             // 에러 발생 시 처리
             Debug.LogError("Error: " + request.error);
@@ -57,34 +114,27 @@ public class EasterEggData : MonoBehaviour
             if (response.success)
             {
 
-                Debug.Log("로그인 성공");
-                Debug.Log("username: " + response.data.username);
-                Debug.Log("userId: " + response.data.userId);
-                Debug.Log("email: " + response.data.email);
-                Debug.Log("lastStage: " + response.data.lastStage);
-                UserData.instance.email = response.data.email;
-                UserData.instance.userName = response.data.username;
-                UserData.instance.userId = response.data.userId;
-                UserData.instance.lastStage = response.data.lastStage;
+                Debug.Log("이스터에그 획득 성공");
+                Debug.Log("skinImgUrl: " + response.data.skinImgUrl);
+                Debug.Log("description: " + response.data.description);
+                Debug.Log("attributes: " + response.data.attributes.character);
+                Debug.Log("attributes: " + response.data.attributes.color);
+                Debug.Log("duplicated: " + response.data.duplicated);
+                EasterEggInfo easterEggInfo = new EasterEggInfo();
+                easterEggInfo.skinImgUrl = response.data.skinImgUrl;
+                easterEggInfo.description = response.data.description;
+                Attributes attributes = new Attributes();
+                attributes.character = response.data.attributes.character;
+                attributes.color = response.data.attributes.color;
+                easterEggInfo.attributes = attributes;
+                easterEggInfo.duplicated = response.data.duplicated;
 
-                //SceneManager.LoadScene("LobbyScene");
-
-                //// 로그인 성공 시 사용자 정보 저장 등 처리 (PlayerPrefs 사용)
-                //PlayerPrefs.SetInt("userId", response.data.userId);
-                //PlayerPrefs.SetString("username", response.data.username);
-                //PlayerPrefs.SetString("email", response.data.email);
-                //PlayerPrefs.SetInt("lastStage", response.data.lastStage);
-                //PlayerPrefs.Save();
-
-                // 로그인 성공 시 팝업창 닫기 또는 다른 씬으로 전환
-                //loginPanel.SetActive(false);
             }
             else
             {
-                // 로그인 실패 시 오류 메시지 표시
-                //errorMessageText.text = "로그인 실패: " + response.message;
-                //ShowLoginFailPopup();  // 로그인 실패 팝업창 띄우기
+                Debug.Log("이스터에그 획득 실패");
             }
         }
+        Destroy(gameObject);
     }
 }
