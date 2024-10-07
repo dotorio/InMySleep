dea45633a9a240862d40e030c7b20564eb13a453
어지민 <djwlalssla@naver.com>
2024-10-07 15:58:05 +0900

    [GAME] feat: 게임 클리어 닉네임 추가
    Former-commit-id: 7694cc2d386fc949bbf873cabd2ed3e566a68cd0

--- game/InMyDream/Assets/Scripts/Manager/Stages/GameClearManager.cs
+++ game/InMyDream/Assets/Scripts/Manager/Stages/GameClearManager.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 using Photon.Pun;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI; // 씬 전환을 위해 필요
+using TMPro;
 
 public class GameClearManager : MonoBehaviour
 {
     public Button clearBtn;
+    public TextMeshProUGUI userName;
     void Start()
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+        userName.text = UserData.instance.userName + "님";
 
         // 방장이 클리어 버튼 클릭 시 로비로 이동
         if (PhotonNetwork.LocalPlayer.IsMasterClient)
